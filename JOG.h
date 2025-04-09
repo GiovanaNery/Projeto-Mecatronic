7060d071c814dc821b98b4b58f5c76d3f0bfbde8
GiovanaNery <giovanamnery@gmail.com>
2025-04-09 19:56:37 +0000

    jog

--- JOG.h
+++ JOG.h
@@ -5,11 +5,17 @@
 #include <string>
 
 // declaração de funções e variaveis
+// funções direções dos eixos 
 void z_menos();
 void z_mais();
 void x_menos();
 void x_mais();
 void y_menos();
 void y_mais();
+// funções para desligar o motor
+void z_desliga();
+void x_desliga();
+void y_desliga();
+
 #endif
         
