86deba348bd0aab5ebc999e8f2e52ccd967c8960
viniciusrap <viniciusrap@al.insper.edu.br>
2025-04-15 20:44:01 +0000

    vini

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include "JOG.h"
 #include "mbed.h"
 #include "Referenciamento.h"
+Ponto3D = {0, 0, 0};  // x = 0, y = 0, z = 0
+struct Ponto3D {
+    int x = 0;
+    int y = 0;
+    int z = 0;
+};
+
 
 int main() {
+    Ponto3D
     // Inicializa o sistema
     referenciar_EixoZ();   // <- função que você criou com setup de posição e volume
 
